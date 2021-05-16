cd5fa4a3bcd58d216dd8441bc6731ef9f51c540c
KenNguyen-dev <74177525+KenNguyen-dev@users.noreply.github.com>
2021-05-16 13:35:47 +0700

    Update dbconfig.js

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,15 +1,19 @@
-const sql = require('mssql/msnodesqlv8')
+//const sql = require('mssql/msnodesqlv8')
+const sql = require("mssql");
 //msnodesqlv8 module is requiered for Windows Authentication without using Username and Password
 
 var config = {
-    database: 'voucherDB',
-    server: 'localhost',
-    driver: 'msnodesqlv8',
-    options: {
-      trustedConnection: true
-    }
-}
+  user: "sa",
+  password: "123",
+  database: "voucherDB",
+  server: "localhost",
+  //driver: 'msnodesqlv8',
+  options: {
+    trustedConnection: true,
+    enableArithAort: true,
+  },
+};
 
-const db = new sql.ConnectionPool(config)
+const db = new sql.ConnectionPool(config);
 
 module.exports = db;
