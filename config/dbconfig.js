233039d8649e1164e3b6c6f4c9e058a6d06c7196
KenNguyen-dev <74177525+KenNguyen-dev@users.noreply.github.com>
2021-05-18 19:36:16 +0700

    Change db

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,12 +1,20 @@
 const sql = require("mysql");
 
 var config = {
-  host: "localhost",
-  user: "root",
-  password: "123bebong",
-  database: "voucherdb",
+  host: "us-cdbr-east-03.cleardb.com",
+  user: "bfaa9fc6657f84",
+  password: "931583b3",
+  database: "heroku_72fcec997c0e882",
   insecureAuth: true,
 };
+
+// var config = {
+//   host: "localhost",
+//   user: "root",
+//   password: "123bebong",
+//   database: "voucherdb",
+//   insecureAuth: true,
+// };
 
 const db = new sql.createConnection(config);
 
